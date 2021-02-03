0683718e0bfbb0d37aa7dbf8de83da7dab212fa1
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-03 16:49:20 +0100

    add key and unique id, users can register in mailChimp

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
@@ -16,10 +17,44 @@ app.post("/", function (req, res) {
   const lastName = req.body.lName;
   const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-})
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/31ad91484d";
+
+  const options = {
+    method: "POST",
+    auth: "Jamie:027610e85acb5dbc5b5aae7a777595c5-us7"
+  };
+
+  const request = https.request(url, options, function(response) {
+    response.on("date", function (data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, function () {
   console.log("server is running");
 });
+
+// Api Key
+// 027610e85acb5dbc5b5aae7a777595c5-us7
+
+//unique id for audience
+//31ad91484d
