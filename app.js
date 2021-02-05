553b4806731523e57bef57d75415ebe67284f437
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-05 15:49:57 +0100

    final commit, ready to deploy - API key needs to be changed for full functionality

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post("/", function (req, res) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html")
     } else {
-      res.sendFile(__dirname + "/failure.html")
+      res.sendFile(__dirname + "/failure.html")t
     }
 
     response.on("date", function (data) {
